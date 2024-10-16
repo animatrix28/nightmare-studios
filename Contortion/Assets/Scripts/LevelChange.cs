e434fbe38183836fa3d47fffc482bd7e06d4f7ba
Eugenia <eugeniadru@gmail.com>
2024-10-16 16:38:12 -0700

    fixed bugs, implemented play again loop

--- Contortion/Assets/Scripts/LevelChange.cs
+++ Contortion/Assets/Scripts/LevelChange.cs
@@ -21,6 +21,7 @@ public class LevelChange : MonoBehaviour
             {
                 SceneManager.LoadScene("PlayAgain");
             }
+            Debug.Log(currentLevel);
         }
     }
 
