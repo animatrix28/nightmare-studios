83312543224f5df8bb45670acbe2cad0432265b8
animatrix0007 <animeshs@usc.edu>
2024-11-04 18:46:41 -0800

    Added respawn time and fixed last level

--- Contortion/Assets/Scripts/PlayerKiller.cs
+++ Contortion/Assets/Scripts/PlayerKiller.cs
@@ -1,36 +1,45 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
 
 public class PlayerKiller : MonoBehaviour
 {
+    public GameObject deathMessageUI;
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Spikes")
         {
-            Respawn();
+            StartCoroutine(RespawnWithDelay());
         }
 
         if (collision.gameObject.tag == "Crusher")
         {
             Rigidbody2D rb = GetComponent<Rigidbody2D>();
             Rigidbody2D crusherRb = collision.collider.attachedRigidbody;
-           
+
             float forceThreshold = 100f;
             float crusherVelocityMagnitude = crusherRb.velocity.magnitude;
             float collisionForce = crusherVelocityMagnitude * crusherRb.mass;
 
-            Debug.Log("Force: " + collisionForce + "Vel:" + crusherVelocityMagnitude);
+
 
             if (collisionForce > forceThreshold && crusherVelocityMagnitude > 0)
             {
                 Debug.Log("High force collision detected with Crusher. Force: " + collisionForce + "Vel:" + crusherVelocityMagnitude);
-                Respawn();
+                StartCoroutine(RespawnWithDelay());
             }
         }
     }
 
-    void Respawn()
+    IEnumerator RespawnWithDelay()
     {
+        Time.timeScale = 0;
+        deathMessageUI.SetActive(true);
+
+        yield return new WaitForSecondsRealtime(2);
+        
+        Time.timeScale = 1;
         Scene currentScene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(currentScene.name);
     }
