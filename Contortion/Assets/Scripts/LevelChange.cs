33f362ef4d05f58b0e2a2d50688bdc3a07d94672
Shaurya <shauryabhatnagar1@gmail.com>
2024-11-08 00:02:37 -0800

    Fixed level change script and death message

--- Contortion/Assets/Scripts/LevelChange.cs
+++ Contortion/Assets/Scripts/LevelChange.cs
@@ -17,15 +17,25 @@ public class LevelChange : MonoBehaviour
         int nextSceneIndex = currentSceneIndex + 1;
         // Debug.Log(currentSceneIndex+"wefwbijuwbi");
 
-        if (currentSceneIndex < SceneManager.sceneCountInBuildSettings - 1)
+
+        if (currentSceneIndex < 3)
         {
 
-            string nextSceneName = $"Level_{currentSceneIndex}";
+            string nextSceneName = $"Tutorial_{currentSceneIndex + 1}";
             // Debug.Log(nextSceneIndex);
             SceneManager.LoadScene(nextSceneName);
         }
+        // else if (currentSceneIndex < SceneManager.sceneCountInBuildSettings - 1)
+        else if (currentSceneIndex < 9)
+        {
+            string nextSceneName = $"Level_{currentSceneIndex - 2}";
+            Debug.Log(currentSceneIndex);
+            SceneManager.LoadScene(nextSceneName);
+
+        }
         else
         {
+
             SceneManager.LoadScene("PlayAgain");
         }
     }
